661ebd404c47dd65b9aef211b12ea0b8211edd6d
rishabh <rishabhj945@gmail.com>
2021-09-21 13:04:00 +0530

    accio Dynamic Programming

--- LEVELUP/DP/TargetSet.java
+++ LEVELUP/DP/TargetSet.java
@@ -19,6 +19,16 @@ public class TargetSet {
         }
     }
 
+    public static void display2D(boolean[][] dp) {
+
+        for (boolean[] a : dp) {
+            for (boolean e : a) {
+                System.out.print(e + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static int Permutation(int[] arr, int target, int[] dp) {
         if (target == 0)
             return dp[target] = 1;
@@ -91,6 +101,7 @@ public class TargetSet {
 
     }
 
+    //////////////////////////////////// Subset Sum////////////////////
     static int isSubsetSumHelper(int n, int[] arr, int sum, int[][] dp) {
 
         if (sum == 0) {
@@ -103,7 +114,7 @@ public class TargetSet {
 
         if (dp[n][sum] != -1)
             return dp[n][sum];
-        int ans=0;
+        int ans = 0;
         if (sum - arr[n - 1] >= 0) {
 
             ans = isSubsetSumHelper(n - 1, arr, sum - arr[n - 1], dp);
@@ -113,8 +124,6 @@ public class TargetSet {
             return dp[n][sum] = 1;
         }
         ans = isSubsetSumHelper(n - 1, arr, sum, dp);
-
-      
 
         return dp[n][sum] = ans;
     }
@@ -150,7 +159,31 @@ public class TargetSet {
 
     }
 
-    static Boolean isSubsetSum(int N, int arr[], int sum) {
+    /// Reverse Engg for finding all solutions
+    public static int isSubsetSum_backEngg(int n, int[] arr, int sum, boolean[][] dp, String ans) {
+
+        if (sum == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        if (n == 0) {
+
+            return 0;
+        }
+
+        int count = 0;
+        if (dp[n - 1][sum])
+            count += isSubsetSum_backEngg(n - 1, arr, sum, dp, ans);
+
+        if (sum - arr[n - 1] >= 0 && dp[n - 1][sum - arr[n - 1]]) {
+            count += isSubsetSum_backEngg(n - 1, arr, sum - arr[n - 1], dp, arr[n - 1] + " " + ans);
+        }
+
+        return count;
+    }
+
+    public static Boolean isSubsetSum(int N, int arr[], int sum) {
 
         int[][] dp = new int[N + 1][sum + 1];
 
@@ -159,8 +192,21 @@ public class TargetSet {
 
         int ans = isSubsetSumHelper(N, arr, sum, dp);
         display2D(dp);
+
         return ans == 1 ? true : false;
     }
+
+    public static int isSubsetSum_tab(int N, int[] arr, int sum) {
+        boolean[][] dp = new boolean[N + 1][sum + 1];
+
+        isSubsetSumHelper_tab(N, arr, sum, dp);
+        return isSubsetSum_backEngg(N, arr, sum, dp, "");
+
+        // display2D(dp);
+        // return ans;
+    }
+
+    //////////////////////////////// KnapSack...................................
 
     static int knapSack(int W, int[] wt, int[] val, int n, int[][] dp) {
 
@@ -217,6 +263,72 @@ public class TargetSet {
         return knapSack(W, wt, val, n, dp);
     }
 
+    static int knapSack_with_duplicates(int n, int w, int val[], int wt[], int[][] dp) {
+
+        if (n == 0 || w == 0) {
+
+            return dp[n][w] = 0;
+        }
+
+        if (dp[n][w] != -1)
+            return dp[n][w];
+
+        int count = knapSack_with_duplicates(n - 1, w, val, wt, dp);
+
+        if (w - wt[n - 1] >= 0) {
+
+            count = Math.max(count, knapSack_with_duplicates(n, w - wt[n - 1], val, wt, dp) + val[n - 1]);
+        }
+
+        return dp[n][w] = count;
+    }
+
+    static int knapSack_with_duplicates(int N, int W, int val[], int wt[]) {
+        int[][] dp = new int[N + 1][W + 1];
+
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+
+        return knapSack_with_duplicates(N, W, val, wt, dp);
+    }
+
+    /// find Number of Solutions of a Linear equation using Coeff in Coeff. Array
+    public static int NumberOfSolutions(int[] Coeff, int index, int rhs, int[][] dp) {
+
+        if (rhs == 0) {
+            return dp[index][rhs] = 1;
+        }
+
+        if (index == 0)
+            return dp[index][rhs] = 0;
+
+        if (dp[index][rhs] != -1) {
+            return dp[index][rhs];
+        }
+
+        int count = 0;
+
+        count += NumberOfSolutions(Coeff, index - 1, rhs, dp);
+        if (rhs - Coeff[index - 1] >= 0)
+            count += NumberOfSolutions(Coeff, index, rhs - Coeff[index - 1], dp);
+
+        return dp[index][rhs] = count;
+    }
+
+    public static int NumberOfSolutions(int[] Coeff, int rhs) {
+
+        int[][] dp = new int[Coeff.length + 1][rhs + 1];
+
+        for (int[] a : dp)
+            Arrays.fill(a, -1);
+
+        int ans = NumberOfSolutions(Coeff, Coeff.length, rhs, dp);
+        display2D(dp);
+        return ans;
+
+    }
+
     public static void main(String[] args) {
 
         int[] arr = { 2, 3, 5, 7 };
@@ -235,7 +347,8 @@ public class TargetSet {
 
         // System.out.println(Combination_dp(arr, target, dp2));
         // display(dp2);
-        isSubsetSum(arr.length, arr, target);
+        System.out.println(isSubsetSum_tab(arr.length, arr, target));
+        // System.out.println(NumberOfSolutions(arr, target));
 
     }
 
