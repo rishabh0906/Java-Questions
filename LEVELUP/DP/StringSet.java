83ff6e2c3b147ddab9f62d1acd61987dd1e1b17e
rishabh <rishabhj945@gmail.com>
2021-09-26 13:33:07 +0530

    newly new

--- LEVELUP/DP/StringSet.java
+++ LEVELUP/DP/StringSet.java
@@ -467,6 +467,48 @@ public class StringSet {
         }
     }
 
+    // Regular Expression Matching
+
+    public static int isMatch_02(String s, String p, int n, int m, int[][] dp) {
+        if (n == 0 && m == 0)
+            return dp[n][m] = 1;
+        if (m == 0)
+            return dp[n][m] = 0;
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        char ch1 = n > 0 ? s.charAt(n - 1) : '$';
+        char ch2 = p.charAt(m - 1);
+
+        if (ch1 != '$' && (ch1 == ch2 || ch2 == '.'))
+            return dp[n][m] = isMatch_02(s, p, n - 1, m - 1, dp);
+        else if (ch2 == '*') {
+            boolean res = false;
+
+            if (n > 0 && m > 1 && (p.charAt(m - 2) == '.' || p.charAt(m - 2) == ch1))
+                res = res || isMatch_02(s, p, n - 1, m, dp) == 1;
+            res = res || isMatch_02(s, p, n, m - 2, dp) == 1;
+
+            return dp[n][m] = res ? 1 : 0;
+        } else
+            return dp[n][m] = 0;
+    }
+
+    public static boolean isMatch_02(String s, String p) {
+        p = RemoveStar(p);
+        int n = s.length();
+        int m = p.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+
+        return isMatch_02(s, p, n, m, dp) == 1;
+
+    }
+
     public static void main(String[] args) {
 
         longestPalindromeSubseq();
