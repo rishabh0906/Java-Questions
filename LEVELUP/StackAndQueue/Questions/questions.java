ae873bf99baf8baade5245e5f679ce0535827774
rishabh <rishabhj945@gmail.com>
2021-10-07 22:53:06 +0530

    stack

--- LEVELUP/StackAndQueue/Questions/questions.java
+++ LEVELUP/StackAndQueue/Questions/questions.java
@@ -154,4 +154,32 @@ public class questions {
         return st.size() == 0;
     }
 
+    public int[] asteroidCollision(int[] asteroids) {
+        LinkedList<Integer> st = new LinkedList<>();
+
+        for (int ele : asteroids) {
+            if (ele > 0) {
+                st.addFirst(ele);
+                continue;
+            }
+            while (st.size() != 0 && st.getFirst() > 0 && st.getFirst() < -ele) {
+                st.removeFirst();
+            }
+
+            if (st.size() != 0 && st.getFirst() == -ele) {
+                st.removeFirst();
+            } else if (st.size() == 0 || st.getFirst() < 0) {
+                st.addFirst(ele);
+            }
+        }
+
+        int[] ans = new int[st.size()];
+        int idx = ans.length - 1;
+        while (st.size() != 0) {
+            ans[idx--] = st.removeFirst();
+        }
+
+        return ans;
+    }
+
 }
