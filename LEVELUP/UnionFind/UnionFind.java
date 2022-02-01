b1fe9d1d0046740c923ff28ae4346a7d08cdb903
rishabh <rishabhj945@gmail.com>
2022-02-01 23:02:49 +0530

    new

--- LEVELUP/UnionFind/UnionFind.java
+++ LEVELUP/UnionFind/UnionFind.java
@@ -4,7 +4,7 @@ public class UnionFind {
 
     public static Scanner scn = new Scanner(System.in);
     private static int[] parent;
-    int[] size;
+    private static int[] size;
     int[] rank;
 
     public static int find(int u) {
@@ -15,6 +15,21 @@ public class UnionFind {
         }
 
         return parent[u] = find(parent[u]);
+    }
+
+    public static void merge(int u, int v) {
+
+        int p1 = find(u);
+        int p2 = find(v);
+        if (p1 == p2)
+            return;
+        if (size[p1] >= size[p2]) {
+            parent[p2] = p1;
+            size[p1] += size[p2];
+        } else {
+            parent[p1] = p2;
+            size[p2] += size[p2];
+        }
     }
 
     public static void main(String[] args) {
@@ -42,7 +57,7 @@ public class UnionFind {
 
         }
 
-        int[] disconnectedNodes = new int[redundant.size()+1];
+        int[] disconnectedNodes = new int[redundant.size() + 1];
         int indx = 0;
         for (int i = 1; i <= n; i++) {
             int p = find(i);
@@ -59,6 +74,49 @@ public class UnionFind {
                     + disconnectedNodes[i + 1]);
         }
 
+    }
+
+    public int minMalwareSpread(int[][] graph, int[] initial) {
+        int n = graph.length;
+        parent = new int[n];
+        size =new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (graph[i][j] == 1) {
+
+                    merge(i, j);
+                }
+            }
+        }
+
+        int[] freq = new int[n];
+        Arrays.sort(initial);
+        for (int i = 0; i < initial.length; i++) {
+            freq[find(initial[i])]++;
+        }
+
+        int ans = initial[0];
+
+        int mx = 0;
+
+        for (int i = 0; i < initial.length; i++) {
+            int p = find(initial[i]);
+
+            if (freq[p] > 1)
+                continue;
+
+            if (size[p] > mx) {
+                mx = size[p];
+                ans = initial[i];
+            }
+        }
+
+        return ans;
     }
 
 }
--- LEVELUP/practice/practice.java
+++ LEVELUP/practice/practice.java
@@ -1,6 +1,8 @@
 
 import java.util.*;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
 public class practice {
 
     public static Scanner scn = new Scanner(System.in);
@@ -151,6 +153,24 @@ public class practice {
         int r = scn.nextInt();
         Circle c = new Circle(r);
 
+    }
+
+    public int value(int[][] shops) {
+        int n = shops.length;
+        int[][] dp = new int[n][3];
+
+        dp[0][0] = shops[0][0];
+        dp[0][1] = shops[0][1];
+        dp[0][2] = shops[0][2];
+
+        for (int i = 1; i < n; i++) {
+
+            dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][2]) + shops[i][0];
+            dp[i][1] = Math.max(dp[i - 1][2], dp[i - 1][0]) + shops[i][1];
+            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][0]) + shops[i][2];
+        }
+
+        return Math.max(dp[n - 1][0], Math.max(dp[n - 1][1], dp[n - 1][2]));
     }
 
 }
